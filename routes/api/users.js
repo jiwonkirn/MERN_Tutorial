35921b4c40686c89185a66c4d74f7bdb65bd20c4
Kimjiwon <jhd1925@gmail.com>
2019-01-02 22:28:34 +0900

    feat: add regster & login validate function

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 // Load Input Validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // Load User model
 const User = require("../../models/User");
@@ -74,6 +75,12 @@ router.post("/register", async (req, res) => {
 // @desc    Login user / Returning JWT Token
 // @access  Public
 router.post("/login", async (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -82,7 +89,8 @@ router.post("/login", async (req, res) => {
 
   // Check for user
   if (!user) {
-    return res.status(404).json({ email: "Email not found" });
+    errors.email = "Email not found";
+    return res.status(404).json(errors);
   }
 
   // Check password
@@ -100,7 +108,8 @@ router.post("/login", async (req, res) => {
       });
     });
   } else {
-    return res.status(400).json({ password: "Password incorrect" });
+    errors.password = "Password incorrect";
+    return res.status(400).json(errors);
   }
 });
 
