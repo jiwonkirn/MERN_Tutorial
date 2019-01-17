24550a740839820f69ad3d29c32f0f926b8aec11
Kimjiwon <jhd1925@gmail.com>
2019-01-17 23:44:09 +0900

    feat: Create redux store

---
+++ client/src/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const initialState = {};
+
+const middleware = [thunk];
+
+// first parameter is reducer, second is initial state, third is middleware
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
